323c5d6e33be9bcfda2d35434317fd010dda9d4b
Eric Obermühlner <eobermuhlner@gmail.com>
2018-02-18 19:37:53 +0100

    add UI to turtle screen - [Random] button

--- core/src/ch/obermuhlner/planet/engine/screen/turtle/TurtleScreen.kt
+++ core/src/ch/obermuhlner/planet/engine/screen/turtle/TurtleScreen.kt
@@ -6,19 +6,25 @@ import ch.obermuhlner.planet.engine.render.UberShaderProvider
 import ch.obermuhlner.planet.engine.screen.turtle.model.SpaceShipGenerator
 import ch.obermuhlner.planet.engine.util.Random
 import com.badlogic.gdx.Gdx
+import com.badlogic.gdx.InputMultiplexer
 import com.badlogic.gdx.assets.AssetManager
 import com.badlogic.gdx.graphics.Color
 import com.badlogic.gdx.graphics.GL20
 import com.badlogic.gdx.graphics.PerspectiveCamera
 import com.badlogic.gdx.graphics.g2d.BitmapFont
 import com.badlogic.gdx.graphics.g2d.SpriteBatch
+import com.badlogic.gdx.graphics.g2d.TextureAtlas
 import com.badlogic.gdx.graphics.g3d.Environment
 import com.badlogic.gdx.graphics.g3d.ModelInstance
 import com.badlogic.gdx.graphics.g3d.attributes.ColorAttribute
 import com.badlogic.gdx.graphics.g3d.environment.DirectionalLight
 import com.badlogic.gdx.graphics.g3d.utils.CameraInputController
+import com.badlogic.gdx.scenes.scene2d.Stage
+import com.badlogic.gdx.scenes.scene2d.ui.Skin
 import ktx.app.KtxScreen
 import ktx.app.use
+import ktx.scene2d.*
+import ktx.actors.*
 import kotlin.math.roundToInt
 
 class TurtleScreen : KtxScreen {
@@ -39,17 +45,19 @@ class TurtleScreen : KtxScreen {
         update()
     }
 
-    val modelInstances = mutableListOf<ModelInstance>().apply {
-        val generator = SpaceShipGenerator()
-        add(ModelInstance(generator.generate(Turtle(), Random())))
+    val stage = Stage()
+
+    val controller = CameraInputController(camera)
+
+    val modelInstances = mutableListOf<ModelInstance>()
+
+    init {
+        Gdx.input.inputProcessor = InputMultiplexer(stage, controller)
+        prepareStage()
     }
 
     val fogColor = Color(0.0f, 0.0f, 0.2f, 1f)
     val ambientLightColor = Color(0.2f, 0.2f, 0.3f, 1.0f)
-
-    val controller = CameraInputController(camera).apply {
-        Gdx.input.inputProcessor = this
-    }
 
     val environment = Environment().apply {
         set(ColorAttribute(ColorAttribute.AmbientLight, ambientLightColor))
@@ -57,8 +65,44 @@ class TurtleScreen : KtxScreen {
         add(DirectionalLight().set(Color.LIGHT_GRAY, -1f, -0.8f, -0.2f))
     }
 
+    fun prepareStage() {
+        val skin = Skin()
+        skin.addRegions(TextureAtlas(Gdx.files.internal("data/ui/uiskin.atlas")))
+        skin.add("small-font", BitmapFont())
+        skin.add("default-font", BitmapFont())
+        skin.add("large-font", BitmapFont())
+        skin.load(Gdx.files.internal("data/ui/uiskin.json"))
+
+        Scene2DSkin.defaultSkin = skin
+
+        val root = table {
+            button {
+                label("Random")
+            }.onClick { createContent() }
+        }
+
+        root.top()
+        root.left()
+        root.setFillParent(true)
+        stage.addActor(root)
+
+        createContent()
+    }
+
+    fun createContent() {
+        val generator = SpaceShipGenerator()
+        modelInstances.clear()
+        modelInstances.add(ModelInstance(generator.generate(Turtle(), Random())))
+    }
+
+    override fun resize(width: Int, height: Int) {
+        super.resize(width, height)
+        stage.viewport.update(width, height)
+    }
+
     override fun render(delta: Float) {
         controller.update()
+        stage.act()
 
         Gdx.gl.glViewport(0, 0, Gdx.graphics.width, Gdx.graphics.height)
         Gdx.gl.glClearColor(fogColor.r, fogColor.g, fogColor.b, fogColor.a);
@@ -68,6 +112,8 @@ class TurtleScreen : KtxScreen {
         modelBatch.render(modelInstances, environment)
         modelBatch.end()
 
+        stage.draw()
+
         spriteBatch.use {
             val fps = (1f/delta).roundToInt()
             font.draw(it, "${modelInstances.size} model instances, $fps FPS", 10f, 10 + font.lineHeight)
@@ -75,6 +121,7 @@ class TurtleScreen : KtxScreen {
     }
 
     override fun dispose() {
+        stage.dispose()
         font.dispose()
         spriteBatch.dispose()
 
